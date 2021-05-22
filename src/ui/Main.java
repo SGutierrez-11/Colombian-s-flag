6b8a1eb6a17f26dd5a161620d13ffb6f11d4e9af
Santiago Gutiérrez <santicoguvi02@gmail.com>
2021-05-21 21:05:56 -0500

    Now, the last change

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,17 +9,21 @@ public class Main {
     public static final String BLUE= "\u001B[44m";
     public static final String RED= "\u001B[41m";
     public static final String EMPTY= "\u001B[0m";
-	
+    
+    private static Thread_Flag flagY;
+    private static Thread_Flag flagB;
+    private static Thread_Flag flagR;
+    
 	public static void main(String[] args) throws InterruptedException {
 		
-		Thread_Flag flagY = new Thread_Flag(0, 60, 0, 8, 30, YELLOW+" ");
-		Thread_Flag flagB = new Thread_Flag(0, 60, 8, 12, 100, BLUE+" ");
-		Thread_Flag flagR = new Thread_Flag(0, 60, 12, 16, 110, RED+" ");
+		flagY = new Thread_Flag(0, 60, 0, 8, 30, YELLOW+" ");
+		flagB = new Thread_Flag(0, 60, 8, 12, 100, BLUE+" ");
+		flagR = new Thread_Flag(0, 60, 12, 16, 110, RED+" ");
 		
 		
 		flagY.start();
 		flagB.start();
 		flagR.start();
-		System.out.print(" "+EMPTY);
+		
 	}
 }
